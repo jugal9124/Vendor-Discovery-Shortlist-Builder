0b10567e72437bfa9d398d24618b981cef5a0774
jugal@9124 <786ugal@gmail.com>
2026-06-16 08:06:48 +0530

    frontend working fine

--- FrontEnd/src/api/client.js
+++ FrontEnd/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL, timeout: 30000 });
+const api = axios.create({ baseURL: "/api", timeout: 30000 });
 
 api.interceptors.response.use(
   (r) => r,
