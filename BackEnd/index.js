b5c724870f64fcbaddf2a4e8debc07ae396e59d4
jugal@9124 <786ugal@gmail.com>
2026-06-16 07:31:04 +0530

    adit index.js cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,12 +15,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: [
-      "https://vendor-discovery-shortlist-builder-bw98.onrender.com/",
-    ],
-    credentials: true,
-  }));
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
 
